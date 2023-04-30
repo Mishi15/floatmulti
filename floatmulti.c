4ecbc48b434710431655e858fc3d344a61ae4133
Mishi15 <122174407+Mishi15@users.noreply.github.com>
2023-04-30 11:55:49 +0530

    Create floatmulti.c

---
+++ floatmulti.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    float a, b, multi;
+    printf("Enter the first number =  ");
+    scanf("%f", &a);
+    printf("Enter the second no  = ");
+    scanf("%f", &b);
+
+    multi = a * b;
+    printf("%f", multi);
+    return 0;
+}
